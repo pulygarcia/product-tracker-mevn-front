41990d04ee65e7b518be30f71d997ea2fac57042
puly garcia <pulygarcia09@gmail.com>
2024-05-08 21:45:08 -0300

    giving position to the alert

--- src/components/Alert.vue
+++ src/components/Alert.vue
@@ -14,8 +14,17 @@
 
 <template>
   <v-alert
+    class="fixed"
     :text="message"
     :title="title"
     :type="type"
   ></v-alert>
 </template>
+
+<style>
+    .fixed{
+        position: fixed;
+        top: .5rem;
+        right: 1rem
+    }
+</style>
