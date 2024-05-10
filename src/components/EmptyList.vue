c4a3eb23475bbea81335f0fef7a0a1799de8e0e1
puly garcia <pulygarcia09@gmail.com>
2024-05-10 20:52:06 -0300

    feedback component when products list is empty added

---
+++ src/components/EmptyList.vue
@@ -0,0 +1,64 @@
+<script setup>
+    import { RouterLink } from 'vue-router';
+</script>
+
+<template>
+  <div class="d-flex flex-column justify-space-center align-center mt-10 ga-1">
+    <div class="container">
+      <div class="folder">
+        <div class="top"></div>
+        <div class="bottom"></div>
+      </div>
+      <div class="title">List is empty, start adding products in <RouterLink :to="{name: 'control-panel'}">Control</RouterLink></div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+    .container {
+  width: fit-content;
+  gap: 10px
+}
+
+.folder {
+  width: min-content;
+  margin: auto;
+  animation: float 2s infinite linear;
+}
+
+.folder .top {
+  background-color: #FF8F56;
+  width: 60px;
+  height: 12px;
+  border-top-right-radius: 10px;
+}
+
+.folder .bottom {
+  background-color: #FFCE63;
+  width: 100px;
+  height: 70px;
+  box-shadow: 5px 5px 0 0 #283149;
+  border-top-right-radius: 8px;
+}
+
+.container .title {
+  font-size: .9em;
+  color: #283149;
+  text-align: center;
+  margin-top: 15px;
+}
+
+@keyframes float {
+  0% {
+    transform: translatey(0px);
+  }
+
+  50% {
+    transform: translatey(-25px);
+  }
+
+  100% {
+    transform: translatey(0px);
+  }
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,7 @@
 <script setup>
   import {formatCurrency} from '../helpers/index.js'
   import { useProductsStore } from '@/stores/productsStore';
+  import EmptyList from '../components/EmptyList.vue'
 
   const productsStore = useProductsStore();
 
@@ -11,58 +12,60 @@
   <main class="pa-3 w-100 scroll">
     <h1 class="mt-3">Products</h1>
 
-    <div class="mt-5 w-50">
-          <v-select
-            label="Filter by category"
-            :items="productsStore.categories"
-            v-model="productsStore.selectedCategory"
-          ></v-select>
+    <div v-if="productsStore.products.length">
+      <div class="mt-5 w-50">
+            <v-select
+              label="Filter by category"
+              :items="productsStore.categories"
+              v-model="productsStore.selectedCategory"
+            ></v-select>
+      </div>
+      <v-table class="mt-6">
+      <thead>
+        <tr>
+          <th class="text-left text-indigo-darken-2 font-weight-bold text-subtitle-1">
+            Name
+          </th>
+          <th class="text-left text-indigo-darken-2 font-weight-bold text-subtitle-1">
+            Brand
+          </th>
+          <th class="text-left text-indigo-darken-2 font-weight-bold text-subtitle-1">
+            Price
+          </th>
+          <th class="text-left text-indigo-darken-2 font-weight-bold text-subtitle-1">
+            Category
+          </th>
+          <th class="text-left text-indigo-darken-2 font-weight-bold text-subtitle-1">
+            Quantity
+          </th>
+          <th class="text-left text-indigo-darken-2 font-weight-bold text-subtitle-1">
+            ID
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="product in productsStore.products"
+          :key="product._id"
+        >
+          <td>{{ product.name }}</td>
+          <td>{{ product.brand }}</td>
+          <td>US<span class="font-weight-bold">{{ formatCurrency(product.price) }}</span></td>
+          <td>{{ product.category }}</td>
+          <td
+            :class="product.quantity <= 0 ? 'bg-red-darken-1 text-center font-weight-bold' : ''"
+          >
+          {{ product.quantity <= 0 ? 'Not available' : product.quantity }}
+          </td>
+      
+          <td>{{ product._id }}</td>
+        </tr>
+      </tbody>
+        </v-table>
     </div>
 
-    <v-table class="mt-6">
-    <thead>
-      <tr>
-        <th class="text-left text-indigo-darken-2 font-weight-bold text-subtitle-1">
-          Name
-        </th>
-        <th class="text-left text-indigo-darken-2 font-weight-bold text-subtitle-1">
-          Brand
-        </th>
-        <th class="text-left text-indigo-darken-2 font-weight-bold text-subtitle-1">
-          Price
-        </th>
-        <th class="text-left text-indigo-darken-2 font-weight-bold text-subtitle-1">
-          Category
-        </th>
-        <th class="text-left text-indigo-darken-2 font-weight-bold text-subtitle-1">
-          Quantity
-        </th>
-        <th class="text-left text-indigo-darken-2 font-weight-bold text-subtitle-1">
-          ID
-        </th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr
-        v-for="product in productsStore.products"
-        :key="product._id"
-      >
-        <td>{{ product.name }}</td>
-        <td>{{ product.brand }}</td>
-        <td>US<span class="font-weight-bold">{{ formatCurrency(product.price) }}</span></td>
+  <EmptyList v-else/>
 
-        <td>{{ product.category }}</td>
-
-        <td
-          :class="product.quantity <= 0 ? 'bg-red-darken-1 text-center font-weight-bold' : ''"
-        >
-        {{ product.quantity <= 0 ? 'Not available' : product.quantity }}
-        </td>
-        
-        <td>{{ product._id }}</td>
-      </tr>
-    </tbody>
-  </v-table>
   </main>
 </template>
 <style>
